370e19eb7d254276feb951eefa276d6673dc692d
Ricardo <ricardo-jsb@hotmail.com>
2024-01-31 12:01:33 +0100

    style: css and html adjusts

--- decrypt.js
+++ decrypt.js
@@ -1,6 +1,8 @@
 function wordDecrypt() {
   const resultElement = document.getElementById("resultado");
+
   let encryptedWord = resultElement.innerText;
+  resultElement.innerText = "";
 
   let decryptedWord = encryptedWord.replace("Texto Criptografado:\n", "");
   decryptedWord = decryptedWord.replace(/enter/g, "e");
@@ -8,8 +10,9 @@ function wordDecrypt() {
   decryptedWord = decryptedWord.replace(/ai/g, "a");
   decryptedWord = decryptedWord.replace(/ober/g, "o");
   decryptedWord = decryptedWord.replace(/ufat/g, "u");
+  decryptedWord = decryptedWord.replace("Texto Descriptografado:\n", "");
 
-  console.log(encryptedWord);
   resultElement.innerText = "Texto Descriptografado:\n" + decryptedWord;
+  console.log(decryptedWord);
   return decryptedWord;
 }
